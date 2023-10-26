a857669bbfd3cf5fa4cea9e13a0eeff04d4f2ca3
shivanigandhi8013 <sg8013@nyu.edu>
2023-10-26 01:25:17 -0400

    1

--- DNSServer.py
+++ DNSServer.py
@@ -96,7 +96,7 @@ dns_records = {
     },
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: str(input_string),
+        dns.rdatatype.TXT: str(encrypted_value),
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
